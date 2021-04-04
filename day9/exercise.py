c942f0012df758e9d0428f6a0723eefc15a859a5
Hedgehuug <47126622+Hedgehuug@users.noreply.github.com>
2021-04-04 18:51:47 +0200

    Finished exercises(04/04/21)

--- day9/exercise.py
+++ day9/exercise.py
@@ -10,4 +10,17 @@ display them as follows:
 list_of_names = ["tim",'tom','ben','bin','bim']
 
 for counter,names in enumerate(list_of_names,start=1):
-    print(counter+'. '+names)
+    print(counter,'. ',names,sep='')
+#I actually did not know how to change the separator for print, but I made an educated guess
+
+
+"""
+Use the enumerate function on a string (your name) and print
+each character with its corresponding index
+"""
+
+my_name = 'Ben Dover'
+
+for counter,letter in enumerate(my_name):
+    print(str(counter)+':',letter)
+#I don't get this one, it's the same question as before, I did it in a different way I guess
